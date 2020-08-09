75be7aacde7c592b428c0eb7485d451ac4ff25af
vishgirase95 <67498551+vishgirase95@users.noreply.github.com>
2020-08-09 22:40:36 +0530

    updated

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$("h1").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
